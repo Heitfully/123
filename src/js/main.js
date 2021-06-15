b726d2c9afc4cf96f7f9c1472e2bb03bf19104ea
heitfully <Heitgully@mail.ru>
2021-06-16 01:05:07 +0200

    burger

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-//here will by JavaScript code ...
+document.querySelector('.header__burger').addEventListener('click', () => {
+    document.querySelector('.header__nav').classList.toggle('active');
+  });
+
+  document.querySelector('.header__burger').addEventListener('click', () => {
+    document.querySelector('.header__burger').classList.toggle('active');
+  });
